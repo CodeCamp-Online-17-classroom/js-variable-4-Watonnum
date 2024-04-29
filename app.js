15c827a2b1a4134b0c6d2ff529606f6c5ff04da0
Watonnum <adamcorktail@gmail.com>
2024-04-29 21:09:27 +0700

    Done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+const firstName = "Watanyu";
+let lastName = "Onnum";
+var nickname = "Pae";
+
+const firstName = "sompong";
+let lastName = "onpranee";
+var nickname = "tao";
+
+birthDate = 28 / 03 / 1998;
